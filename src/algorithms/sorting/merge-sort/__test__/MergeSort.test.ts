f45542eab2a3ce3aa0492ec6a1d3096f61001f42
Evgeny Kolotov <evgenykolotov.it@gmail.com>
2023-04-06 21:59:32 +0300

    Алгоритм сортировки "Сортировка слиянием"

--- src/algorithms/sorting/merge-sort/__test__/MergeSort.test.ts
+++
@@ -1,8 +0,0 @@
-import MergeSort from '../MergeSort';
-import SortTester from '../../SortTester';
-
-describe('Тестирование алгоритма сортировка слиянием', () => {
-  it('Тестирование сортировки массива с положительными числами', () => {
-    SortTester.testReturnsSort(MergeSort);
-  });
-});
